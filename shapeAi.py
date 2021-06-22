8c07b179e785bf7ad87b6560b2f050f52275a9f1
varsha0211 <86284212+varsha0211@users.noreply.github.com>
2021-06-22 12:31:57 +0530

    codes for shapeAi project

---
+++ shapeAi.py
@@ -0,0 +1,35 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = 'a920f263ee3e225b496105d340cb9462'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+
+with open('output.txt','w') as f:
+    f.write("-------------------------------------------------------------\n")
+    f.write("Weather Stats for - "  + format(location.upper()) +  " | " + format(date_time) + "\n" ) 
+    f.write("-------------------------------------------------------------\n")
+    f.write("\nCurrent temperature is: " + format(temp_city) + " deg C\n")
+    f.write("Current weather desc  : " + format(weather_desc) +  "\n" )
+    f.write("Current Humidity      : " + format(hmdt) + " %\n")
+    f.write("Current wind speed    : " + format(wind_spd) + " kmph\n")
